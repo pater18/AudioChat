1fa68c97eba76fdcdf079cbe20434d791f80e996
pater18 <55958169+pater18@users.noreply.github.com>
2019-10-08 08:46:46 +0200

    Afspil lyd
    Jeg har sat det grundlæggende ind for at spille en lyd

--- Sound.h
+++ Sound.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <math.h>
+
+namespace sound {
+#define TWOPI 6.283185307
+
+	short SineWave(double time, double freq, double amp) {
+		short result;
+		double tpc = 44100 / freq;
+		double cycles = time / tpc;
+		double rad = TWOPI * cycles;
+		short amplitude = 32767 * amp;
+		result = amplitude * sin(rad);
+
+		return result;
+
+
+	}
+
+}
+
--- main.cpp
+++ main.cpp
@@ -1,11 +1,30 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include "Sound.h"
+#include <iostream>
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
+
+	sf::SoundBuffer buffer;
+	std::vector<sf::Int16> samples;
+
+	for (int i = 0; i < 44100; i++)
+	{
+		samples.push_back(sound::SineWave(i, 440, 0.9));
+	}
+
+	buffer.loadFromSamples(&samples[0], samples.size(), 1, 441000);
+
+	sf::Sound sound;
+	sound.setBuffer(buffer);
+	sound.play();
+
+
 
 	while (window.isOpen())
 	{
