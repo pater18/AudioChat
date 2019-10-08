981641f22967978b5b44e37411a2a439555fe13b
pater18 <55958169+pater18@users.noreply.github.com>
2019-10-08 09:12:00 +0200

    Ændring

--- Sound.h
+++ Sound.h
@@ -4,18 +4,26 @@
 namespace sound {
 #define TWOPI 6.283185307
 
-	short SineWave(double time, double freq, double amp) {
-		short result;
-		double tpc = 44100 / freq;
-		double cycles = time / tpc;
-		double rad = TWOPI * cycles;
+	short SineWave(double time, double freq1, double freq2, double amp) {
+		short result1;
+		double tpc1 = 44100 / freq1;
+		double cycles1 = time / tpc1;
+		double rad1 = TWOPI * cycles1;
 		short amplitude = 32767 * amp;
-		result = amplitude * sin(rad);
+		result1 = amplitude * sin(rad1);
 
-		return result;
+		short result2;
+		double tpc2 = 44100 / freq2;
+		double cycles2 = time / tpc2;
+		double rad2 = TWOPI * cycles2;
+		result2 = amplitude * sin(rad2);
 
+
+
+		return result1 + result2;
 
 	}
+	
 
 }
 
