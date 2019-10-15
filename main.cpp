eab5a41b417d966ede3c1230b2e068b3c1227e8a
VinterHviid <48584265+VinterHviid@users.noreply.github.com>
2019-10-15 21:21:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 #include <fstream>
 #include <bitset>
 #include <string> 
+#include <thread>
+#include <chrono>
 
 
 
@@ -16,24 +18,20 @@
 
 int main()
 {
-	std::vector<int> test_a_streng;				//En vektor der skal indeholde den binære sekvens for en streng
-	StrToBit(test_a_streng, "Hello World");		//Beskeden der skal sendes og den vektor den skal ligge i som 1 og 0. 
+	//std::vector<int> test_a_streng;				//En vektor der skal indeholde den binære sekvens for en streng
+	//StrToBit(test_a_streng, "Hello World");		//Beskeden der skal sendes og den vektor den skal ligge i som 1 og 0. 
 
 
-	sf::SoundBuffer buffer;
-	sf::Sound sound;
-	std::vector<sf::Int16> test;				
+	//sf::SoundBuffer buffer;
+	//sf::Sound sound;
+	//std::vector<sf::Int16> test;				
 
-	message(test, 11025, test_a_streng);		//Tager besked vektoren med 1 og 0 og lægger det i en ny vektor, som kan læses af SFML. Hver tone bliver sendt i 1 sekund = 44100. 
-
-
-	buffer.loadFromSamples(&test[0], test.size(), 1, 44100);
-	sound.setBuffer(buffer);
-	sound.play();
-	
+	//message(test, 11025, test_a_streng);		//Tager besked vektoren med 1 og 0 og lægger det i en ny vektor, som kan læses af SFML. Hver tone bliver sendt i 1 sekund = 44100. 
 
 
-
+	//buffer.loadFromSamples(&test[0], test.size(), 1, 44100);
+	//sound.setBuffer(buffer);
+	//sound.play();
 
 
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
@@ -53,9 +51,26 @@ int main()
 				window.close();
 		}
 
-		//window.clear();
-		/*window.draw(shape);
-		window.display();*/
+		if (!sf::SoundBufferRecorder::isAvailable())
+		{
+			std::cout << "Error";
+		}
+
+		sf::SoundBufferRecorder recorder;
+		recorder.start();
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		const sf::SoundBuffer& buffer = recorder.getBuffer();
+		recorder.stop();
+
+
+
+		const sf::Int16* samples = buffer.getSamples();
+		std::size_t count = buffer.getSampleCount();
+
+		for (std::size_t i = 30000; i < 30500; i++)
+			std::cout << samples[i] << " ";
+		
+		break;
 	}
 
 	return 0;
