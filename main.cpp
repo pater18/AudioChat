53c3668560061f7f8ac2d7dc6b57d8e691b2dffa
mahas18 <55984147+mahas18@users.noreply.github.com>
2019-11-05 12:11:42 +0100

    med receiver delen

--- main.cpp
+++ main.cpp
@@ -13,23 +13,15 @@
 #include <thread>
 #include <chrono>
 
-
-
 int main()
 {
 
 
 	sf::Event event;
 
-	//koder.StrToBit("hej med dig ");
-	//koder.CRC(32);
-	//koder.message(5000);					//Tager besked vektoren med 1 og 0 og l�gger det i en ny vektor, som kan l�ses af SFML. Hver tone bliver sendt i 1 sekund = 44100. 
-
 
 	sf::SoundBuffer buffer;
 	sf::Sound sound;
-
-
 
 
 	sf::RenderWindow window(sf::VideoMode(1000, 800), "SFML works!");
@@ -42,27 +34,33 @@ int main()
 	rectangleSend.setFillColor(sf::Color(128, 128, 128));
 	rectangleSend.setPosition(850, 700);
 
-	float widthOfText = 0;
+	double widthOfText = 0;
 	sf::RectangleShape rectangleBesked(sf::Vector2f(100, 35));
 
-	
 
+	std::vector<sf::Text> textVector;
+	std::vector<sf::Text> textVector2;
+	std::vector<sf::RectangleShape> rectangleVec;
+	std::vector<sf::RectangleShape> rectangleVec2;
+	std::vector< std::pair< sf::Text, sf::RectangleShape> > textRect;
+	sf::Vector2f xyvec;
 
 
 	int moveText = 80;
-	
 
-	std::string test;
 	sf::Font font;
 
 	font.loadFromFile("blue.ttf");
 
-	
+	std::string test;
 	sf::Text text;
 	text.setFont(font);
 	text.setCharacterSize(20);
 	text.setFillColor(sf::Color::Black);
+	text.setPosition(60, 710);
 
+	double widthOfReceive;
+	std::string receive = "Hej med dig";
 	sf::Text text2;
 	text2.setFont(font);
 	text2.setCharacterSize(20);
@@ -74,24 +72,18 @@ int main()
 	send.setString("Send");
 	send.setCharacterSize(20);
 	send.setFillColor(sf::Color::Black);
+	send.setPosition(880, 725);
 
 	sf::Text newline;
 	newline.setFont(font);
 	newline.setCharacterSize(24);
+	newline.setString("\n");
 
 
-	/*buffer.loadFromSamples(&sinusAmplituder[0], sinusAmplituder.size(), 1, 44100);
-	sound.setBuffer(buffer);
-	sound.play();*/
-
-	std::vector<sf::Text> textVector;
-	std::vector<sf::RectangleShape> rectangleVec;
 
 	while (window.isOpen())
 	{
 
-		text.setPosition(60, 710);
-		send.setPosition(880, 725);
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
@@ -99,41 +91,42 @@ int main()
 			{
 			case sf::Event::Closed:
 				window.close();
-			break;
+				break;
 
 			case sf::Event::MouseButtonPressed:
-				
-					if (event.mouseButton.button == sf::Mouse::Left) 
+
+				if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					if (event.mouseButton.x > 850 && event.mouseButton.x < 950 && event.mouseButton.y > 700 && event.mouseButton.y < 775)
 					{
-						if (event.mouseButton.x > 850 && event.mouseButton.x < 950 && event.mouseButton.y > 700 && event.mouseButton.y < 775) 
+
+						customSound koder;
+						koder.StrToBit(test);
+						koder.CRC(32);
+						koder.message(22150);
+						buffer.loadFromSamples(&koder._customSound[0], koder._customSound.size(), 1, 44100);
+						sound.setBuffer(buffer);
+						sound.play();
+						koder.slet();
+
+						textVector.insert(textVector.begin(), text);
+
+						for (size_t i = 0; i < textVector.size(); i++)
 						{
-							
-							customSound koder;
-							koder.StrToBit(test);
-							koder.CRC(32);
-							koder.message(22150);
-							buffer.loadFromSamples(&koder._customSound[0], koder._customSound.size(), 1, 44100);
-							sound.setBuffer(buffer);
-							sound.play();
-							koder.slet();
-
-							textVector.insert(textVector.begin(), text);
-
-							for (size_t i = 0; i < textVector.size(); i++)
-							{
-								textVector[i].move(0, -moveText);
-
-							}
-						
-							test.clear();
-							break;
+							textVector[i].move(0, -moveText);
 
 						}
-					}
 
-				
+						test.clear();
+						break;
+
+					}
+				}
+
+
 			case sf::Event::KeyPressed:
-				if (event.key.code == sf::Keyboard::Enter) {
+				if (event.key.code == sf::Keyboard::Enter) 
+				{
 					customSound koder;
 					koder.StrToBit(test);
 					koder.CRC(32);
@@ -143,72 +136,119 @@ int main()
 					sound.play();
 					koder.slet();
 
-
+					// move sendte tekst
 					textVector.insert(textVector.begin(), text);
 
 					for (size_t i = 0; i < textVector.size(); i++)
 					{
-					textVector[i].move(0, -moveText);
-					
+						textVector[i].move(0, -moveText);
+
 					}
 
+					// move rectangle
+					for (size_t i = 0; i < textVector.size(); i++)
+					{
+						widthOfText = text.getLocalBounds().width;
+
+						xyvec = textVector[i].getPosition();
+						rectangleVec.insert(rectangleVec.begin(), sf::RectangleShape(sf::Vector2f(0, 0)));
+						rectangleVec[i].setPosition(xyvec.x, xyvec.y + moveText);
+						rectangleVec[i].setFillColor(sf::Color(128, 128, 128));
+						rectangleVec[i].setSize(sf::Vector2f(widthOfText + 5, 35));
+
+
+						for (size_t i = 0; i < rectangleVec.size(); i++)
+						{
+
+							rectangleVec[i].move(0, -moveText);
+
+
+						}
+
+
+				}
+
+
+
 					test.clear();
+
+					// receiver delen
+					text2.setString(receive);
+					widthOfReceive = text2.getLocalBounds().width;
+					text2.setPosition(1000 - widthOfReceive - 50, 710 - moveText);
+
+					textVector2.insert(textVector2.begin(), text2);
+
+					for (size_t i = 1; i < textVector2.size(); i++)
+					{
+						textVector2[i].move(0, -moveText);
+
+					}
+
+					receive.clear();
+
 					break;
 				}
-			
+
 
 			case sf::Event::TextEntered:
 				if (event.type == sf::Event::TextEntered)
 				{
 					if (event.text.unicode < 128 && (event.text.unicode != 13))
 						test += (char)event.text.unicode;
-					
+
 					widthOfText = text.getLocalBounds().width + 50;
 					sf::Vector2f vector;
 					vector = rectangle.getPosition();
 					float xRectangle = vector.x;
 					float widthRect = xRectangle + rectangle.getSize().x;
 
-					if (widthOfText + 40 >= widthRect) {
+					if (widthOfText + 40 >= widthRect) 
+					{
 						test += "\n";
 						widthOfText = 50;
+						
 					}
 
-				text.setString(test);
-				break;
+					text.setString(test);
+
+
+					break;
+
 				}
 			}
 
-			for (size_t i = 0; i < textVector.size(); i++)
+			window.clear(sf::Color::White);
+			window.draw(rectangle);
+			window.draw(rectangleSend);
+			window.draw(rectangleBesked);
+
+			for (auto obj : rectangleVec)
 			{
-				sf::Vector2f xyvec;
-				xyvec = textVector[i].getPosition();
-				widthOfText = textVector[i].getLocalBounds().width;
-				//rectangleVec.insert(rectangleVec.begin(), sf::Vector2f(widthOfText + 5, 35));
-				rectangleVec[i].setPosition(xyvec.x, xyvec.y);
-				rectangleBesked.setSize(sf::Vector2f(widthOfText + 5, 35));
-				rectangleBesked.setFillColor(sf::Color(128, 128, 128));
-				
+				window.draw(obj);
 			}
 
-		window.clear(sf::Color::White);
-		window.draw(rectangle);
-		window.draw(rectangleSend);
-		window.draw(text);
-		window.draw(rectangleBesked);
-		for (auto obj : textVector)
-		{
-			window.draw(obj);
+			window.draw(text);
+			window.draw(text2);
+
+			for (auto obj : textVector)
+			{
+				window.draw(obj);
+			}
+
+			for (auto obj : textVector2)
+			{
+				window.draw(obj);
+			}
+
+			window.draw(send);
+			window.display();
+
 		}
-				
-		window.draw(send);
-		window.display();
-
-			}
 
 
-}
-	
+	}
+
 
 	return 0;
 }
