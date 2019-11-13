915e8d9953ac5cac932c3ef38a7b391cf65b169a
tstaa18 <staal_66@hotmail.com>
2019-11-13 08:24:13 +0100

    Update Protokol.cpp

--- Protokol.cpp
+++ Protokol.cpp
@@ -35,8 +35,12 @@ std::vector<sf::Int16> Protokol::sendProtokol(std::vector<std::vector<sf::Int16>
 
 std::vector<sf::Int16> Protokol::modtagetProtokol(std::vector<sf::Int16> modtaget)
 {
+	int startFlag[8] = { 1,1,1,1,0,0,0,0 };
+
 	if ((modtaget[7] == 0) && (modtaget[8] == 0) && (modtaget[9] == 0) && (modtaget[10] == 0) && (modtaget[11] == 0) && (modtaget[12] == 0) && (modtaget[13] == 0) && (modtaget[14] == 0))
-{
+	{
+
+		ack.insert(ack.begin(), startFlag, startFlag + 8);
 	//Så send x+1 til transmitter
 	ack.push_back(0);
 	ack.push_back(0);
@@ -46,10 +50,12 @@ std::vector<sf::Int16> Protokol::modtagetProtokol(std::vector<sf::Int16> modtage
 	ack.push_back(0);
 	ack.push_back(0);
 	ack.push_back(1);
+	ack.insert(ack.end(), startFlag, startFlag + 8);
 	//Lav ack om til lyd og send til encoder
 }
 else if ((modtaget[7] == 0) && (modtaget[8] == 0) && (modtaget[9] == 0) && (modtaget[10] == 0) && (modtaget[11] == 0) && (modtaget[12] == 0) && (modtaget[13] == 0) && (modtaget[14] == 1))
 {
+		ack.insert(ack.begin(), startFlag, startFlag + 8);
 	//Så send x til transmitter
 	ack.push_back(0);
 	ack.push_back(0);
@@ -60,6 +66,7 @@ else if ((modtaget[7] == 0) && (modtaget[8] == 0) && (modtaget[9] == 0) && (modt
 	ack.push_back(0);
 	ack.push_back(0);
 	//Lav ack om til lyd og send til encoder
+	ack.insert(ack.end(), startFlag, startFlag + 8);
 }
 	return ack;
 
