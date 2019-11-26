0117a57d2e65147c8684d9988012b014acdb9e2a
pater18 <55958169+pater18@users.noreply.github.com>
2019-11-26 11:30:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,11 +23,14 @@
 
 int main()
 {
-
-	customSound test;
-	std::vector<sf::Int16> lyde{15, 0, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,15 ,15,0 };
-	test.playSound(test.testAfLyd(44100 * g_sendeTid, lyde));
-
+	CustomRecorder recorder;
+	recorder.start(g_samplingFreq);
+	sf::sleep(sf::seconds(g_sendeTid * 16 + 3));
+	recorder.stop();	
+	//customSound test;
+	//std::vector<sf::Int16> lyde{ 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
+	//test.playSound(test.testAfLyd(44100 * g_sendeTid, lyde));
+	
 
 
 	Userinterface ui;
